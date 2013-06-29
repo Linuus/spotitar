8ed75f21cb18d719c3a25f97a00cbddd9cd63af8
Linus Pettersson <linus.pettersson@gmail.com>
2013-06-29 22:03:35 +0200

    A lot more stuff done...

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,33 @@
 require([
   '$api/models',
-], function(models) {
+  '$views/buttons',
+  '$views/throbber#Throbber',
+  '$views/popup'
+], function(models, buttons, Throbber, popup) {
   'use strict';
+
   // Get the currently-playing track
   models.player.load('track').done(updateCurrentTrack);
   // Update the DOM when the song changes
   models.player.addEventListener('change:track', updateCurrentTrack);
 
+  var Tracks = [];
+
   function updateCurrentTrack() {
+    Tracks = [];
+    $("#tab-area").text('');
     updateTrackHeader();
+    createVersionsButton();
     updateTab();
+  }
+
+  function createVersionsButton() {
+    if($(".versions-button").size() == 0) {
+      var btn = buttons.CustomButton.withClass('versions-button');
+      btn.setLabel("Versions");
+      btn.setAccentuated(true);
+      $('#top-bar .versions-button-wrapper').append(btn.node);
+    }
   }
 
   function updateTrackHeader() {
@@ -28,7 +46,7 @@ require([
 
   function updateTab() {
     var search = createSearchString();
-    getData("http://app.ultimate-guitar.com/search.php?search_type=title&page=1&iphone=1&value="+search, 'xml', fetchTab);
+    getData("http://app.ultimate-guitar.com/search.php?search_type=title&page=1&iphone=1&value="+search, 'xml', fetchTabs);
   }
 
   function createSearchString() {
@@ -40,26 +58,49 @@ require([
     return str.replace(/ /g, '+').replace(/&/g, 'and');
   }
 
-  function fetchTab(data) {
+  function fetchTabs(data) {
     var results = $(data).find('results').attr('count');
     if(results > 0) {
-      var res = $(data).find('result:first');
-      getData($(res).attr('url'), 'html', displayTab);
+      $(data).find('result').each(function() {
+        Tracks.push({
+          id: $(this).attr('id'),
+          name: $(this).attr('name'),
+          url: $(this).attr('url'),
+          version: $(this).attr('version'),
+          type: $(this).attr('type'),
+          type_2: $(this).attr('type_2')
+        });
+      });
+      displayTab(Tracks[0]);
     } else {
-      $("#tab-area").text("No tab was found");
+      renderTab("No tracks were found");
     }
   }
 
-  function displayTab(data) {
-    $("#tab-area").html(data);
+  function displayTab(track) {
+    getData(track.url, 'html', renderTab);
+  }
+
+  function renderTab(tab) {
+    var formatted = formatTab(tab);
+    $("#tab-area").html(formatted);
+    
+  }
+
+  function formatTab(tab) {
+    var nTab = tab.replace(/\[ch\]/g, '<span class="chord">');
+    nTab = nTab.replace(/\[\/ch\]/g, "</span>");
+    return nTab;
   }
 
   function getData(url, dataType, callback) {
-    console.log("getData");
+    var tabArea = document.getElementById('tab-area');
+    var throbber = Throbber.forElement(tabArea);
     $.ajax({
       url: url,
       dataType: dataType,
       success: function(data) {
+        throbber.hide();
         callback(data);
       },
       error: function(data) {
